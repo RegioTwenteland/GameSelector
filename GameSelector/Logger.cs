6cc00c96092a9bbd89fa614a8623fb876c8a380e
david <david@comman.nl>
2023-03-25 15:18:44 +0000

    Change NFCReaderView to UserInputView, remove IView interface

---
+++ GameSelector/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSelector
+{
+    internal static class Logger
+    {
+        static Logger()
+        {
+        }
+
+        public static void LogError(object who, string message)
+        {
+            Log("ERROR", ConsoleColor.DarkRed, who, message);
+        }
+
+        public static void LogInfo(object who, string message)
+        {
+            Log("INFO", ConsoleColor.White, who, message);
+        }
+
+        private static void Log(string logType, ConsoleColor color, object who, string message)
+        {
+            var original = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine($"[{logType}] {who.GetType().Name}: {message}");
+            Console.ForegroundColor = original;
+        }
+    }
+}
---
+++ GameSelector/Views/UserInputView.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameSelector.Views
+{
+    internal class UserInputView
+    {
+        private readonly External.NFCReader nfcReader;
+
+        public UserInputView()
+        {
+            nfcReader = new External.NFCReader();
+
+
+            Logger.LogInfo(this, "watching");
+            nfcReader.Watch();
+        }
+
+        public string GetData()
+        {
+            try
+            {
+                if (nfcReader.Connect())
+                {
+                    //Writing   
+                    //NFC.WriteBlock("Some string data that will not exceed block limit", "2"); //public bool WriteBlock(String Text, String Block)
+                    string str = Encoding.Default.GetString(nfcReader.ReadBlock("4"));
+
+                    return str;
+
+                }
+                else
+                {
+                    Logger.LogError(this, "Could not connect to NFC card");
+                    return string.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(this, ex.ToString());
+                return string.Empty;
+            }
+        }
+
+        public void SetData(string data)
+        {
+            try
+            {
+                if (nfcReader.Connect())
+                {
+                    //Writing   
+                    DateTime now = DateTime.Now;
+                    nfcReader.WriteBlock(now.ToString(), "4"); //public bool WriteBlock(String Text, String Block)
+
+
+                }
+                else
+                {
+                    Logger.LogError(this, "Could not connect to NFC card");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(this, ex.ToString());
+            }
+        }
+    }
+}
